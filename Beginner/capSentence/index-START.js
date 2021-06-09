b1beb7aff487a00a630d18a75e41feb502f55f89
Sai <snaadira@gmail.com>
2021-06-08 21:31:20 -0400

    completed solution with prep

--- Beginner/capSentence/index-START.js
+++ Beginner/capSentence/index-START.js
@@ -2,13 +2,27 @@
 Given a sentence containing two or more words, 
 return the equivalent of the sentence when capitalised. E.g
   capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!' 
+
+  P: string
+  R: string with first letter capitalized
+  E: 'hello, world' => 'Hello World'
+  P: split string into an array 
+     iterate through arr and capitalize firs letter
+     slice arr to keep rest of letters lowercase
+     join array back into string
 */
 
 
 
 
 const capSentence = text => {
+  let textArr = text.toLowerCase().split(' ')
+  let wordArr = textArr.map(el => el[0].toUpperCase() + el.slice(1))
+  return wordArr.join(' ')
 }
+
+capSentence('hello world')
+
 
 
 
